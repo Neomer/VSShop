b2641627fd80c26db2776527e98d88c8fe0e2382
Vinokurov Kirill <vinokurov@elewise.com>
2018-02-09 17:00:57 +0400

    feature/sale

---
+++ Shop.SDK/Models/SaleItemModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.SDK.Models
+{
+    /// <summary>
+    /// Элемент покупки
+    /// </summary>
+    public class SaleItemModel
+    {
+        public virtual ProductModel Product { get; set; }
+        public virtual ConsignmentItemModel Consignment { get; set; }
+        public virtual SaleModel Sale { get; set; }
+        public virtual double Count { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            var t = obj as SaleItemModel;
+            if (t == null)
+                return false;
+            if (Product == t.Product && Consignment == t.Consignment && Sale == t.Sale)
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Product.Name + "|" + Consignment.Consignment.ID.ToString() + "|" + Sale.ID.ToString()).GetHashCode();
+        }
+    }
+}
---
+++ Shop.SDK/Models/SaleModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.SDK.Models
+{
+    /// <summary>
+    /// Продажа
+    /// </summary>
+    public class SaleModel : IIdentifyed
+    {
+        public virtual DateTime CreationDate { get; set; }
+        public virtual UserModel Buyer { get; set; }
+        public virtual string Address { get; set; }
+        public virtual PaymentType PaymentType { get; set; }
+        public virtual IList<SaleItemModel> Items { get; set; }
+    }
+
+    public enum PaymentType
+    {
+        Cash
+    }
+}
