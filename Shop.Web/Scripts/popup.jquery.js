0f6f86b917fbff510fbf4fe1742630cf3b49bb15
Vinokurov Kirill <vinokurov@elewise.com>
2018-02-19 12:03:43 +0400

    feature/basket

--- Shop.Web/Scripts/popup.jquery.js
+++ Shop.Web/Scripts/popup.jquery.js
@@ -2,15 +2,18 @@
     $.fn.Popup = function (options) {
         var settings = $.extend({
             'caption': '',
-            'content': ''
+            'content': '',
+            'defaultClass': 'popUp-default'
         }, options);
 
         var instance = this;
         var rootElement = $(this);
         var titleElement = rootElement.find('.popUp-header > span');
         var contentElement = rootElement.find('.popUp-body');
+        var headerElement = rootElement.find('.popUp-header');
+        var headerClass = headerElement.attr('class');
 
-        this.open = function (title, content) {
+        this.open = function (title, content, style) {
             titleElement.html(title);
             contentElement.html(content);
             var w = $(document).width() * 0.5 - rootElement.width() * 0.5;
@@ -19,6 +22,13 @@
                 left: w + 'px',
                 top: h + 'px'
             });
+            headerElement.attr('class', headerClass);
+            if (style != null && style != 'undefined') {
+                headerElement.addClass(style);
+            } else {
+                headerElement.addClass(settings['defaultClass']);
+            }
+
             $(".popUp-blockscreen").css({
                 width: $(document).width(),
                 height: $(document).height()
